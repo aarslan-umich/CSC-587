4356861e6a1aac1873f4329647c1c791627a60cf
aarslan-umich <aarslan@umich.edu>
2025-09-09 23:24:32 -0400

    Add files via upload

---
+++ Lab1_Ali Arslan.R
@@ -0,0 +1,9 @@
+#Student Name : Ali ARSLAN
+#UMid :  05986056
+#email: aarslan@umich.edu
+#Class : CSC587
+#Instructor :Dr. Halil Bisgin 
+
+experiment= as.factor(c("yes","no","no","no"))
+class(experiment)
+table(experiment)
